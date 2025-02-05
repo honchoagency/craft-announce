05eb7fafea229b91c0b5f26ae3f6d63c0fca21c1
Martin Drahony <martin@honcho.agency>
2025-02-05 16:48:08 +0000

    config added

---
+++ config.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    // Enable the alert banner for all users.
+    'bannerEnabled' => true,
+
+    // Banner Text
+    'bannerText' => '',
+
+    // Banner Link Text
+    'bannerLinkText' => '',
+
+    // Banner Link
+    'bannerLink' => '',
+
+    // Enable the login modal
+    'loginModalEnabled' => true,
+
+    // Disable the login modal for admins
+    'loginModalAdminDisabled' => false,
+
+    // Title of the login modal
+    'loginModalTitle' => "",
+
+    // Body Text of the login modal
+    'loginModalBodyText' => "",
+
+    // Link Button Text of the login modal
+    'loginModalLinkText' => "",
+
+    // Link Button URL of the login modal
+    'loginModalLink' => "",
+
+    // Continue Button Text of the login modal
+    'loginModalContinueButtonText' => "",
+
+    // Continue Button URL of the login modal
+    'loginModalContinueButtonURL' => "",
+];
