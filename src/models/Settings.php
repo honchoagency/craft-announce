074fdee75845bd9080a73a5fc39b39d8fed3b35a
Martin Drahony <martin@honcho.agency>
2025-02-12 13:01:41 +0000

    property name changes to align better with field labels

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -11,14 +11,13 @@ use craft\validators\UrlValidator;
 class Settings extends Model
 {
     // Public Properties
-    public string $loginModalTitle = '';
-    public bool $loginModalEnabled = false;
-    public bool $loginModalAdminEnabled = true;
-    public string $loginModalBodyText = '';
-    public string $loginModalLinkText = '';
-    public string $loginModalLink = '';
-    public string $loginModalContinueButtonText = '';
-    public string $loginModalContinueButtonURL = '';
+    public string $modalTitle = '';
+    public bool $modalEnabled = false;
+    public string $bodyText = '';
+    public string $linkButtonText = '';
+    public string $linkButtonUrl = '';
+    public string $buttonText = '';
+    public string $buttonRedirectUrl = '';
     public bool $bannerEnabled = false;
     public string $bannerText = '';
     public string $bannerLinkText = '';
@@ -31,8 +30,7 @@ class Settings extends Model
     protected function defineRules(): array
     {
         return array_merge(parent::defineRules(), [
-            [['loginModalTitle', 'bannerText'], 'required'],
-            [['loginModalLink','bannerLink','loginModalContinueButtonURL'], UrlValidator::class],
+            [['linkButtonUrl','bannerLink','buttonRedirectUrl'], UrlValidator::class],
         ]);
     }
 }
