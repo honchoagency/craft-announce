77facde1c1464230a688f8f4ac79580fea95613a
Martin Drahony <martin@honcho.agency>
2025-02-12 15:05:51 +0000

    config merge added to apply them

--- src/services/Settings.php
+++ src/services/Settings.php
@@ -2,10 +2,10 @@
 
 namespace honchoagency\craftannounce\services;
 
-use Craft;
 use yii\base\Component;
 use honchoagency\craftannounce\models\Settings as SettingsModel;
 use honchoagency\craftannounce\records\Settings as SettingsRecord;
+use honchoagency\craftannounce\Plugin;
 
 
 /**
@@ -35,6 +35,23 @@ class Settings extends Component
             $settings->bannerText = $settingsRecord->bannerText;
             $settings->bannerLinkText = $settingsRecord->bannerLinkText;
             $settings->bannerLink = $settingsRecord->bannerLink;
+        }
+
+        // get config and merge settings
+        $config = Plugin::getInstance()->pluginConfig;
+
+        if ($config) {
+            $settings->modalTitle = $config['modalTitle'] ?? $settings->modalTitle;
+            $settings->modalEnabled = $config['modalEnabled'] ?? $settings->modalEnabled;
+            $settings->bodyText = $config['bodyText'] ?? $settings->bodyText;
+            $settings->linkButtonText = $config['linkButtonText'] ?? $settings->linkButtonText;
+            $settings->linkButtonUrl = $config['linkButtonUrl'] ?? $settings->linkButtonUrl;
+            $settings->buttonText = $config['buttonText'] ?? $settings->buttonText;
+            $settings->buttonRedirectUrl = $config['buttonRedirectUrl'] ?? $settings->buttonRedirectUrl;
+            $settings->bannerEnabled = $config['bannerEnabled'] ?? $settings->bannerEnabled;
+            $settings->bannerText = $config['bannerText'] ?? $settings->bannerText;
+            $settings->bannerLinkText = $config['bannerLinkText'] ?? $settings->bannerLinkText;
+            $settings->bannerLink = $config['bannerLink'] ?? $settings->bannerLink;
         }
 
         return $settings;
